eee96b47d8ca345d0c61196a7f3a1270a07ee05f
lawal olawale <lawalolawale32@gmail.com>
2022-11-08 12:07:12 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const reset = document.getElementById("reset");
+const btnSubmit = document.getElementById("submit");
+let total = document.getElementById("total");
+
+btnSubmit.addEventListener("click", function () {
+  console.log(reset.innerHTML);
+});
