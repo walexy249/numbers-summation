b591d6208015043faf8bafa147e0ccf5e74cea3d
lawal olawale <lawalolawale32@gmail.com>
2022-11-08 12:12:36 +0100

    added click event to button submit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const reset = document.getElementById("reset");
 const btnSubmit = document.getElementById("submit");
 let total = document.getElementById("total");
+let point = document.getElementById("point");
 
 btnSubmit.addEventListener("click", function () {
-  console.log(reset.innerHTML);
+  console.log(total.value);
+  console.log(point.innerHTML);
 });
